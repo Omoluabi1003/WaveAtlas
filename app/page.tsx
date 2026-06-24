79f1c9c31b16e2b768ca11cafe9bba3a4b0f47a4
Omoluabi1003 <pakiyogun10@gmail.com>
2026-06-24 13:34:44 -0400

    Mount station profile strip on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 import WaveAtlasApp from '@/components/WaveAtlasApp';
+import { StationProfileStrip } from '@/components/station-profile-strip';
 import { fetchStations, getStationInventoryStats } from '@/lib/stations';
-export default async function Home(){ const [stations, inventoryStats] = await Promise.all([fetchStations({ limit:'32', allowFallback:'true' }), getStationInventoryStats()]); return <WaveAtlasApp stations={stations} inventoryStats={inventoryStats}/>; }
+
+export default async function Home(){ const [stations, inventoryStats] = await Promise.all([fetchStations({ limit:'32', allowFallback:'true' }), getStationInventoryStats()]); return <><WaveAtlasApp stations={stations} inventoryStats={inventoryStats}/><StationProfileStrip /></>; }
